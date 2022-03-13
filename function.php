3bc7f3b6ad97300be719b00bf126c1e28568c6a9
Matteo Santini <mattwo3112@gmail.com>
2022-03-13 21:02:07 +0100

    Aggiunta funzione di moltiplicazione

--- function.php
+++ function.php
@@ -4,10 +4,16 @@
 function addizione (n1, n2){
     return n1 + n2;
 }
-/*sottrazione */
+/*sottrazione*/
 
 function sottrazione (n1, n2){
     return n1 - n2;
+}
+
+/*moltiplicazione*/
+
+function moltiplicazione (n1, n2){
+    return n1 * n2;
 }
 
 ?>
